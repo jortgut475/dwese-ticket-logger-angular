0d202d2dee86af471ecc0647eadc2e90c02df9b5
jortgut475 <jortgut475@g.educaand.es>
2025-06-05 11:11:03 +0200

    Actualización de configuración y entornos antes del pull

--- src/enviroments/environments.ts
+++ src/enviroments/environments.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://localhost:8080/api',
+  webSocketUrl: 'http://localhost:8080/ws',
+  webSocketBroker: 'ws://localhost:8080/ws'
 };
